9b82eac5b6b568a237f43c8a3052ef6e7afec87f
Everton Leon <evertonleon07@gmail.com>
2024-12-31 15:32:41 -0300

    feat(route): Criar rota para support

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Admin\SupportController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/support', [SupportController::class, 'index'])->name('support.index');
